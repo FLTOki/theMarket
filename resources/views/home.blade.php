6646024cad3797a2e02d2ba63bbd289dff5985a2
Jim Musyoki <jim.musyoki@strathmore.edu>
2021-05-10 19:37:24 +0300

    landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,46 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row"><!--justify-content-center-->
+        <div class="col-3 p-5">
+            <img src="svg/undraw_web_shopping_dd4l.svg" alt="" width="200px" height="200px">
+        </div>
+        <div class="col-9 p-5 text-center">
+            <div>
+                <h2>THE MARKET</h2>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel magni obcaecati harum, ullam, sed sapiente maxime tempora, id modi sunt nam inventore necessitatibus nihil odio molestias earum. Magni, dolores et.</p>
+            </div>
+            <div>
+                <a href="#" class="btn btn-outline-dark">
+                    SHOP
+                    <img src="svg/cartlogo.svg" alt="">
+                </a>
             </div>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-4 text-center">
+            <h2>Household</h2>
+            <a href="#"><img src="https://images.unsplash.com/photo-1583241475880-083f84372725?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aG91c2Vob2xkfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" class="w-100"></a>
+            <p class="pt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum aperiam ipsum dignissimos repudiandae sed culpa tempora officia doloremque rem in, mollitia impedit similique fuga consequatur quaerat delectus, accusantium consequuntur dolorem.</p>
+            <a href="#" class="btn btn-outline-dark">Shop</a>
+        </div>
+        <div class="col-4 text-center">
+            <h2>Tech</h2>
+            <a href="#"><img src="https://images.unsplash.com/photo-1504610926078-a1611febcad3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" class="w-100"></a>
+            <p class="pt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum aperiam ipsum dignissimos repudiandae sed culpa tempora officia doloremque rem in, mollitia impedit similique fuga consequatur quaerat delectus, accusantium consequuntur dolorem.</p>
+            <a href="#" class="btn btn-outline-dark">Shop</a>
+        </div>
+        <div class="col-4 text-center">
+            <h2>Party</h2>
+            <a href=""><img src="https://images.unsplash.com/photo-1618611627105-74f7567bc6a2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGFsY29ob2x8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" class="w-100"></a>
+            <p class="pt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum aperiam ipsum dignissimos repudiandae sed culpa tempora officia doloremque rem in, mollitia impedit similique fuga consequatur quaerat delectus, accusantium consequuntur dolorem.</p>
+            <a href="#" class="btn btn-outline-dark">Shop</a>
+        </div>
+    </div>
+
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,29 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div>
+                        <img src="/svg/undraw_empty_cart_co35.svg" style="height: 35px;" class="pr-3">
+                    </div>
+                    <div class="pl-3 pt-1">
+                        The Market
+                    </div>
+
+                </a>
+                <a class="navbar-brand d-flex" href="{{ url('#')}}">
+                    <div class="pl-5 pt-1">
+                        Household
+                    </div>
+                </a>
+                <a class="navbar-brand d-flex" href="{{ url('#')}}">
+                    <div class="pl-3 pt-1">
+                        Tech & Gadgets
+                    </div>
+                </a>
+                <a class="navbar-brand d-flex" href="{{ url('#')}}">
+                    <div class="pl-3 pt-1">
+                        Party
+                    </div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
